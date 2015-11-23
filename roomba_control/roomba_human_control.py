b732d6a655e8f86dd0c170c1ae68a6deaf3a4599
jcarlnel <jcnelson@andrew.cmu.edu>
2015-11-23 14:00:19 -0500

    Update roomba control code

--- roomba_control/roomba_human_control.py
+++ roomba_control/roomba_human_control.py
@@ -6,17 +6,22 @@ import RPi.GPIO as GPIO
 
 def sendCommand(connection, command):
     cmd = ""
-    for v in command.split():
-        cmd += chr(int(v))
+    write = False
 
-    if(cmd == "fire"):
-    	#TODO make sure proper syntax for POST request
-    	h.request(fire_ip)
-    	GPIO.output(18, true)
-    	time.sleep(.5)
-    	GPIO.output(18, false)
-    
-    else:
+    for v in command.split():
+        if(cmd == "fire"):
+    		#TODO make sure proper syntax for POST request
+    		h.request(fire_ip)
+    		GPIO.output(18, true)
+    		time.sleep(.5)
+    		GPIO.output(18, false)
+        	
+        else:
+        	write = True
+        	cmd += chr(int(v))
+    	
+
+    if(write):
     	connection.write(cmd)
 
 # getDecodedBytes returns a n-byte value decoded using a format string.
@@ -53,7 +58,8 @@ def main:
 	
 	#create map 2d array
 	dir = 0
-    ang = 0;
+    ang = 0
+    loc = [0,0]
 	map = []
 	for i in range(0, 300):
 		row = []
@@ -66,10 +72,11 @@ def main:
 
 	#setup server connection
 	h = httplib2.Http(".cache")
-	loc_ip = "http://:8000/robot_tag/1/coords/"
-	cmd_ip = "http://:8000/robot_tag/1/command/"
-	fire_ip = "http://:8000/robot_tag/1/fire/"
-    ang_ip = "http://:8000/robot_tag/1/"
+	loc_ip = "http://192.168.43.130:8000/robot_tag/1/coords/"
+	add_coords_ip = "http://192.168.43.130:8000/robot_tag/1/add_coords/"
+	cmd_ip = "http://192.168.43.130:8000/robot_tag/1/command/"
+	fire_ip = "http://192.168.43.130:8000/robot_tag/1/fire/"
+    ang_ip = "http://192.168.43.130:8000/robot_tag/1/add_angles/"
 
 	#connet to roomba
 	port = "/dev/ttyUSB0"
@@ -81,19 +88,30 @@ def main:
 	while True:
 		
 		#get position from the server and parse position
-		resp, content = h.request(loc_ip)
-		coords = content.split()
-		loc = [int(coords[0]), int(coords[1])]
+		#resp, content = h.request(loc_ip)
+		#coords = content.split()
+		#loc = [int(coords[0]), int(coords[1])]
+
+		coords = str(loc[0]).zfill(3) + str(loc[1]).zfill(3)
+		h.request(add_coords_ip + coords + '/')
 
 		#get commands from server
-		resp, content h.request(cmd_ip)
+		resp, content = h.request(cmd_ip)
+		if(content.split()[1] == '145s1s244s1s244'):
+			loc[0] = loc[0] + 1 
+			loc[1] = loc[1] + 1
+
+		if(content.split[1] == '145s254s12s254s12'):
+			loc[0] = loc[0] + 1
+			loc[1] = loc[1] +! 
+
 		sendCommand(connection, content)
 
         #get angle and send it to the server
         connection.write(connection, '20')
         angChange = get16signed(connection)
         ang = ang + angChange
-        resp, content = h.request(ang_ip + ang + "/add_angles/")
+        resp, content = h.request(ang_ip + ang + '/')
 
 		time.sleep(.1)
 		sendCommand('145 0 0 0 0', connection)
