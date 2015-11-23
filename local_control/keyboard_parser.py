b8227a0f613cc51a1f2f88c5e087af85bc5b42e4
Jacob Nelson <jacobnelson@Jacobs-MBP.wv.cc.cmu.edu>
2015-11-23 13:36:23 -0500

    Please work

--- local_control/keyboard_parser.py
+++ local_control/keyboard_parser.py
@@ -1,4 +1,5 @@
 import sys
+import httplib2
 import termios
 import tty
 
@@ -6,54 +7,62 @@ import tty
 # by the user on the keyboard by accessing the system files
 # It will then return the pressed key as a variable
 def getch(): 
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-        if(ch == ' '):
-            exit()
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+	fd = sys.stdin.fileno()
+	old_settings = termios.tcgetattr(fd)
+	try:
+		tty.setraw(sys.stdin.fileno())
+		ch = sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+	return ch
 
 
 def main():
 
 	#connect to server
 	h = httplib2.Http(".cache")
-    cmd_ip = "http://:8000/robot_tag/1/"
+	cmd_ip = "http://192.168.43.130:8000/robot_tag/1/add_command/"
 	
 
-    while True:
+	while True:
 		#get input
-        charIn = getch()
+		charIn = getch()
+
+
 		#parse input into command
-        cmd = '145 '
+
 		#if up 
-        if(charIn == 'w'):
-            cmd =  cmd + '1 244 1 244'
+		if(charIn == 'w'):
+			cmd = '145s1s244s1s244'
 
 		#if down
-        elif(charIn == 's'):
-			cmd = cmd + "254 12 254 12"
+		elif(charIn == 's'):
+			cmd = '145s254s12s254s12'
 			
 		#if left
-        elif(charIn == 'a'):
-			cmd = cmd + '1 244 254 12'
+		elif(charIn == 'a'):
+			cmd = '145s1s244s254s12'
 			
 		#if right
-        elif(charIn == 'd'):
-			cmd = cmd + '254 12 1 244'
+		elif(charIn == 'd'):
+			cmd = '145s254s12s1s244'
 			
-        elif(charIn == "k"):
-            cmd = 'Fire'
+		#if fire
+		elif(charIn == 'l'):
+			cmd = 'fire'
 
-        else:
-            cmd = charIn
+		#exit
+		elif(charIn == 'p'):
+			exit()
 
-        print "cmd: " + cmd
-		h.request(cmd_ip + cmd + "/add_command/")
+		else:
+			cmd = '198765'
+
+		sent = cmd_ip + cmd + '/'
+		print sent
+		resp, contents = h.request(sent)
+		print resp
+	print "escaped"
 
 main()
 
