3e34c9fb08e80f5d42dd6e3c22dc3e9e096ba679
Rahul Ramesh <rahulram@andrew.cmu.edu>
2015-11-23 16:55:05 -0500

    redesigned angle and fire calculations

--- django/lasertag/robot_tag/views.py
+++ django/lasertag/robot_tag/views.py
@@ -12,13 +12,20 @@ def map(request):
 	return render(request, 'map.html', context)
 
 def coords(request, team):
-	coords = Coords.objects.all().order_by('-id')[0]
+	coords = Coords.objects.filter(team=team).order_by('-id')[0]
 	context = {'coords' : str(coords)}
 	return render(request, 'coords.html', context)
 
 def add_coords(request, coords, team):
 	coords = int(coords, 10)
-        latest_coords = Coords(team=team, x=coords/1000, y=coords%1000)
+        latest_coords = Coords.objects.filter(team = team)
+	print latest_coords
+	try:
+		latest_coords = latest_coords[0]
+	except:	
+		latest_coords = Coords.objects.create(team = team, x = 0, y = 0)
+	latest_coords.x = coords/1000
+	latest_coords.y = coords%1000
         latest_coords.save()
         return HttpResponseRedirect(reverse('index'))
 
@@ -28,8 +35,13 @@ def angles(request, team):
 	return render(request, 'angles.html', context)
 
 def add_angles(request, team, angle):
-        latest_angles = Angle(team=team, angle=angle)
-        latest_angles.save()
+	latest_angle = Angle.objects.filter(team = team)
+	try:
+		latest_angles = latest_coords[0]
+	except:	
+		latest_angles = Angle.objects.create(team = team, angle=0)
+        latest_angles.angle = angle
+	latest_angles.save()
         return HttpResponseRedirect(reverse('index'))
 
 def command(request, team):
@@ -39,7 +51,27 @@ def command(request, team):
 
 def add_command(request, team, command):
 	#c = (''.join(command.split("s")))
-        latest_command = Command(team=team, command)
+	dist_step = 50
+	angle_step = 30
+        if command == "145s1s244s254s12":		#left
+		angle = Angle.objects.filter(team=team)[0]
+		angle = (angle + angle_step)%360
+		angle.save()		
+	if command == "145s254s12s1s244": 		#right
+		angle = Angle.objects.filter(team = team)[0]
+		angle = (angle - angle_step)%360
+		angle.save()
+	if command == "145s1s244s1s244":			#front
+		coords = Coords.objects.filter(team = team)[0]
+		coords.x = coords.x + dist_step*math.sin(angle)
+		coords.y = coords.y + dist_step*math.cos(angle)
+
+	if command == "145s254s12s254s12":		#back
+		coords = Coords.objects.filter(team = team)[0]
+		coords.x = coords.x - dist_step*math.sin(angle)
+		coords.y = coords.y - dist_step*math.cos(angle)
+
+	latest_command = Command(team=team, command=command)
         latest_command.save()
         return HttpResponseRedirect(reverse('index'))
 
@@ -58,9 +90,9 @@ def fire(request, team):
 	if (coords2.x - coords1.x != 0):
 		slope = (coords2.y - coords1.y)*1.0/(coords2.x - coords1.x)
 
-	if (math.tan(angle) == 	slope):
+	if (math.tan(angle.angle*1.0) == slope):
 		#target hit
-		fault = Fault(1, n)
+		fault = Fault(fault_type=team, attacker=team)
 		fault.save()
         return HttpResponseRedirect(reverse('index'))
 
